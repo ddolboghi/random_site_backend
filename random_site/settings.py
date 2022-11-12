3156b5b70c878c8c1dec584f795f64d371c8d269
[ddolboghi] <[rhdfyd128@gmail.com]>
2022-11-13 08:08:03 +0900

    secret key 수정

--- random_site/settings.py
+++ random_site/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
+import os
 
-SECRET_KEY = config('SECRET_KEY')
-# cast=bool 이 없으면 False 를 문자열로 인식하게됨.
-DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=a72kvxoyvv3r^v7ese54)je8l+-hk6&4*6o%p+-=ipe2m13*l')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
