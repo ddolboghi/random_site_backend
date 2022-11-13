882b8e56d7b635e018ec56ac98a104a4e340a507
[ddolboghi] <[rhdfyd128@gmail.com]>
2022-11-13 09:34:34 +0900

    setting fix3

--- random_site/settings.py
+++ random_site/settings.py
@@ -129,6 +129,8 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
